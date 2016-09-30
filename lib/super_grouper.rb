d3a767ef70d45b4f56603e895e99a3c3ed397046
Brian Lauber <blauber@arclia.com>
2016-09-30 16:44:53 -0400

    Generalized the key extraction

--- lib/super_grouper.rb
+++ lib/super_grouper.rb
@@ -2,25 +2,41 @@
 module SuperGrouper
   class << self
 
-    def group(things, hierarchy)
-      if hierarchy.any?
-        _recursively_group(things, hierarchy)
+    # Returns a lambda that will retrieve the specified key
+    def key_retriever_for(key)
+      ->(thing) { thing[key] }
+    end
+
+
+    def coerce_extractors(extractors)
+      extractors.map do |x|
+        if x.respond_to? :call
+          x
+        else
+          key_retriever_for(x)
+        end
+      end
+    end
+
+    def group(things, extractors)
+      if extractors.any?
+        _recursively_group(things, coerce_extractors(extractors))
       else
-        raise ArgumentError.new("Expected at least 1 item in grouping hierarchy")
+        raise ArgumentError.new("Expected at least 1 item in grouping extractors")
       end
     end
 
 
-    def _recursively_group(things, hierarchy)
-      return things if hierarchy.empty?
+    def _recursively_group(things, extractors)
+      return things if extractors.empty?
 
-      key = hierarchy.first
-      groupings = things.group_by{|t| t[key]}
+      x = extractors.first
+      groupings = things.group_by(&x)
 
       retval = {}
       groupings.each do |k, v|
-        next_hierarchy = hierarchy[1..-1]
-        retval[k] = _recursively_group(v, next_hierarchy)
+        next_extractors = extractors[1..-1]
+        retval[k] = _recursively_group(v, next_extractors)
       end
 
       retval
